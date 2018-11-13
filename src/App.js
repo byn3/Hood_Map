9c69829978a80d152e29add3b8300c202d1b2ad5
Bryan Leung <bryanleung92@gmail.com>
2018-11-13 15:36:51 -0800

    Just finished part 2 from Yahya Elharony

--- src/App.js
+++ src/App.js
@@ -3,26 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  componentDidMount() {
+    this.renderMap()
   }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAwzRLehoMPKA4cyls3k_wlE6LRHrjEbXo&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+          var map = new window.google.maps.Map(document.getElementById('map'), {
+            center: {lat: 37.781916, lng: -122.407953},
+            zoom: 15
+          });
+        }
+
+
+render() {
+  return (
+    <main>
+      <div id="map"></div>
+    </main>
+  )
+}
+}
+
+function loadScript(url) {
+  var index  = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
